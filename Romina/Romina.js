4c30f0c6ba65b6f082e786f27e218f5319516ac9
Romina <146979639+Rominar9@users.noreply.github.com>
2024-05-06 13:40:56 -0400

    Update Romina.js

--- Romina/Romina.js
+++ Romina/Romina.js
@@ -106,15 +106,15 @@ const promptUser = () => {
     img = document.createElement('img');
     let size = 'width:' + height + 'px;height:' + height + 'px;';
     if (color === 'blue') {
-      img.setAttribute('src', 'img/ninja1.png');
+      img.setAttribute('src', 'img/ninjaA.png');
       img.setAttribute('style', size);
       container.appendChild(img);
     } else if (color === 'green') {
-      img.setAttribute('src', 'imgs/ninja2.png');
+      img.setAttribute('src', 'imgs/ninjaB.png');
       img.setAttribute('style', size);
       container.appendChild(img);
     } else if (color === 'red') {
-      img.setAttribute('src', 'img/ninja3.png');
+      img.setAttribute('src', 'img/ninjaC.png');
       img.setAttribute('style', size);
       conatiner.appendChild(img);
     }
