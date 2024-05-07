f14a0073eb552b92d001aa74253b76c3c6b5e1d0
M.Ramautar <144722341+MarisaR12@users.noreply.github.com>
2024-05-07 13:59:00 -0400

    Update marisa.js

--- Marisa/marisa.js
+++ Marisa/marisa.js
@@ -10,7 +10,7 @@ function Sfx(src) {
   this.sound.setAttribute('preload', 'auto');
   this.sound.setAttribute('controls', 'none');
   this.sound.style.display = 'none';
-  document.body.appendChild(this.sound);
+document.body.appendChild(this.sound);
   this.play = function() {
     this.sound.play();
   };
