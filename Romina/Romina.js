6be0886e6bd09b5f6befe48bb36a3397f3fc0fb3
Romina <146979639+Rominar9@users.noreply.github.com>
2024-05-07 13:27:00 -0400

    Update Romina.js

--- Romina/Romina.js
+++ Romina/Romina.js
@@ -1,4 +1,14 @@
-
+//Loader
+window.addEventListener('load', () => {
+  const loader = document.querySelector('.loader');
+  setTimeout(() => {
+    loader.setAttrubute('style', 'opacity: 0;');
+  }, 500);
+  setTimeout(() => {
+    loader.parentNode.removeChild(loader);
+    document.body.style.overflow = 'initial';
+  }, 1000);
+});
 
 const coinSfx = new Sfx('sfx/Mario Coin.wav');
 const pipeSfx = new Sfx('sfx/Mario Pipe.wav');
